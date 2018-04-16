1bee0f45298052984e72147cb9d7522f941f6fa2
Crawford <907000795@qq.com>
2018-04-16 16:58:08 +0800

    动态组件

--- src/components/Bind.vue
+++ src/components/Bind.vue
@@ -18,9 +18,17 @@
       <input placeholder="Enter your email address" key="email-input">
     </div>
     <button @click="toggle">Toggle input type</button>
+    <div id="dynamic-component-demo">
+      <button v-for="tab in tabs" v-bind:key="tab" v-bind:class="['tab-button', { active: currentTab === tab }]" v-on:click="currentTab = tab">{{ tab }}</button>
+
+      <keep-alive>
+        <component v-bind:is="currentTabComponent" class="tab"></component>
+      </keep-alive>
+    </div>
   </div>
 </template>
 <script>
+import { Post, Archive } from './Dynamic Components/index'
 export default {
   name: 'Bind',
   data () {
@@ -32,15 +40,75 @@ export default {
         { text: '学习 Vue' },
         { text: '整个牛项目' }
       ],
-      loginType: 'username'
-    }
+      loginType: 'username',
+      currentTab: 'Posts',
+      tabs: ['Posts', 'Archive']
+    };
   },
   methods: {
     toggle: function () {
       if (this.loginType === 'username') {
-        this.loginType = ''
-      } else this.loginType = 'username'
+        this.loginType = '';
+      } else this.loginType = 'username';
+    }
+  },
+  computed: {
+    currentTabComponent: function () {
+      return 'tab-' + this.currentTab.toLowerCase()
     }
   }
-}
+};
 </script>
+
+<style>
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
+.tab-button:hover {
+  background: #e0e0e0;
+}
+.tab-button.active {
+  background: #e0e0e0;
+}
+.tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+.posts-tab {
+  display: flex;
+}
+.posts-sidebar {
+  max-width: 40vw;
+  margin: 0;
+  padding: 0 10px 0 0;
+  list-style-type: none;
+  border-right: 1px solid #ccc;
+}
+.posts-sidebar li {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  cursor: pointer;
+}
+.posts-sidebar li:hover {
+  background: #eee;
+}
+.posts-sidebar li.selected {
+  background: lightblue;
+}
+.selected-post-container {
+  padding-left: 10px;
+}
+.selected-post > :first-child {
+  margin-top: 0;
+  padding-top: 0;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 // 注册组件
 Vue.component('my-component', {
   template: '<div>A custom component!</div>'
-})
+});
 
 // 局部注册
 
 const Child = {
   template: '<div>A custom component!!!</div>'
-}
+};
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -22,4 +22,4 @@ new Vue({
     'my-component': Child
   },
   template: '<App/>'
-})
+});
